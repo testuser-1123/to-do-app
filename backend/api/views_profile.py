8fb00f03e5d6b0dae2c2189899abf2dcdcfb2ff6
Alifat <a_sabitov@kbtu.kz>
2026-04-21 00:31:14 +0500

    Create views_profile.py

---
+++ backend/api/views_profile.py
@@ -0,0 +1,18 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .serializers import ProfileSerializer
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = ProfileSerializer(request.user)
+        return Response(serializer.data)
+
+    def patch(self, request):
+        serializer = ProfileSerializer(request.user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
