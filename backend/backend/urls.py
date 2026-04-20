2f89f006fdca441b5eacb4fc7c31482298f6f059
Alifat <a_sabitov@kbtu.kz>
2026-04-21 00:36:54 +0500

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenRefreshView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("api/", include("accounts.urls")),
-    path("api/", include("todos.urls")),
-]
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
